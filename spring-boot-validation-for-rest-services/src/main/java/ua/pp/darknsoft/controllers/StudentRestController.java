d09737afe493bf81effd167c4a475c9f13fdb9e3
andrewdark <samsonov.a@ukr.net>
2018-11-07 17:43:05 +0200

    Customizing Validation Response

--- spring-boot-validation-for-rest-services/src/main/java/ua/pp/darknsoft/controllers/StudentRestController.java
+++ spring-boot-validation-for-rest-services/src/main/java/ua/pp/darknsoft/controllers/StudentRestController.java
@@ -1,16 +1,23 @@
 package ua.pp.darknsoft.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ua.pp.darknsoft.entities.Student;
 import ua.pp.darknsoft.exceptions.StudentNotFoundException;
 import ua.pp.darknsoft.repositories.StudentRepository;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -29,7 +36,18 @@ public class StudentRestController {
         return new ResponseEntity<List<Student>>(students, HttpStatus.OK);
     }
 
-    @GetMapping(value = "getAll") //new approach
+    @GetMapping("/findAll/{id}")
+    public ResponseEntity<Student> findStudent(@PathVariable long id) throws ClassNotFoundException {
+        Optional<Student> student = studentRepository.findById(id);
+
+        if (!student.isPresent())
+            new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<Student>(student.get(), HttpStatus.OK);
+    }
+
+    //new approach
+    @GetMapping(value = "/students")
     public List<Student> getAll() throws ClassNotFoundException {
         List<Student> students = studentRepository.findAll();
         if (students == null || students.isEmpty()) {
@@ -37,5 +55,52 @@ public class StudentRestController {
         }
         return students;
     }
-    
+
+    @GetMapping("/students/{id}")
+    public Resource<Student> retrieveStudent(@PathVariable long id) throws ClassNotFoundException {
+        Optional<Student> student = studentRepository.findById(id);
+
+        if (!student.isPresent())
+            throw new StudentNotFoundException("id-" + id);
+
+        Resource<Student> resource = new Resource<Student>(student.get());
+
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAll());
+
+        resource.add(linkTo.withRel("all-students"));
+
+        return resource;
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudent(@PathVariable long id) {
+        studentRepository.deleteById(id);
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<Object> createStudent(@Valid @RequestBody Student student) {
+        Student savedStudent = studentRepository.save(student);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedStudent.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Object> updateStudent(@Valid @RequestBody Student student, @PathVariable long id) {
+
+        Optional<Student> studentOptional = studentRepository.findById(id);
+
+        if (!studentOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        student.setId(id);
+
+        studentRepository.save(student);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
